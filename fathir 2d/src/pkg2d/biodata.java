d6d89f72ea6393cc40949f39deabd5a18b0e83cc
fadilafatir <116627713+fadilafatir@users.noreply.github.com>
2022-10-25 14:42:55 +0700

    Add files via upload

---
+++ fathir 2d/src/pkg2d/biodata.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2d;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author LK5_22
+ */
+public class biodata {
+    public static void main(String[] args) {
+        String nama, alamat;
+        int nis,usia;
+        double tinggi;
+        
+        Scanner biodata= new Scanner(System.in);
+        System.out.println("Nama saya adalah :");
+        nama= biodata.nextLine();
+        System.out.println("Alamat saya adalah :");
+        alamat= biodata.nextLine();
+        System.out.println("Nis saya adalah :");
+        nis= biodata.nextInt();
+        System.out.println("Usia  saya adalah :");
+        usia = biodata.nextInt();
+        System.out.println("Tinggi saya adalah :");
+        tinggi= biodata.nextDouble();
+        
+        
+        
+        
+    }
+}
